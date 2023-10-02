f5c6b86c54066a1e3fe19cdb398c59774410cdfd
MAKEOUTHILL <rodriisaraiin@gmail.com>
2023-10-02 17:07:50 -0300

    srs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Navbar } from "./components/layout/navbar/Navbar";
-import ItemListContainer from "./components/pages/ItemListContainer/ItemListContainer";
-
+// import ItemListContainer from "./components/pages/ItemListContainer/ItemListContainer";
+import FetchingData from "./components/pages/fetchingData/FetchingData";
 function App() {
-  let saludo = "Hola Coder! Bienvenido";
+  // let saludo = "Hola Coder! Bienvenido";
 
   return (
     <div className="ItemListContainer">
       <Navbar />
-      <ItemListContainer saludo={saludo} />
+      {/* <ItemListContainer saludo={saludo} /> */}
+      <FetchingData />
     </div>
   );
 }
