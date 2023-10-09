5121fde7cb92db1df5edae0ff140e16331a7388d
MAKEOUTHILL <rodriisaraiin@gmail.com>
2023-10-09 16:41:46 -0300

    CAMBIOS

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { Navbar } from "./components/layout/navbar/Navbar";
-// import ItemListContainer from "./components/pages/ItemListContainer/ItemListContainer";
-import FetchingData from "./components/pages/fetchingData/FetchingData";
+// import { Navbar } from "./components/layout/navbar/Navbar";
+import ItemListContainer from "./components/pages/ItemListContainer/ItemListContainer";
+// import FetchingData from "./components/pages/fetchingData/FetchingData";
+
+import { BrowseRouter, Routes, Route } from "react-router-dom";
 function App() {
   // let saludo = "Hola Coder! Bienvenido";
 
   return (
-    <div className="ItemListContainer">
-      <Navbar />
-      {/* <ItemListContainer saludo={saludo} /> */}
-      <FetchingData />
-    </div>
+    // <div className="ItemListContainer">
+    // {/* <Navbar /> */}
+    // {/* <ItemListContainer saludo={saludo} /> */}
+    // {/* <FetchingData /> */}
+    // </div>
+    <BrowseRouter>
+      <Routes>
+        <Route path="\" element={<ItemListContainer />}>
+          {" "}
+        </Route>
+      </Routes>
+    </BrowseRouter>
   );
 }
 
